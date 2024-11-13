66a9c21a237e9a9f3c476f0a7c0cf214dda0a39d
git <florah@gmail.com>
2024-11-13 15:48:44 +0300

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./Components/Header/header";
 import Hero from "./Components/Hero/hero"
 import About from "./Components/About/About";
 import Technologies from "./components/Technologies/Technologies";
-import Projects from "./components/Projects/Projects";
-import Certifications from "./components/certifications/certifications";
-import Contact from "./components/Contacts/Contacts";
+import Projects from "./Components/Projects/Projects";
+import Certifications from "./Components/certifications/certifications";
+import Contact from "./Components/Contacts/Contacts";
 import "./App.css";
 
 function App() {
