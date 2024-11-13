2a8fa46d12636366bd7e9fbcb54080c751718939
git <florah@gmail.com>
2024-11-13 16:11:37 +0300

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./Components/Header/header";
 import Hero from "./Components/Hero/hero"
 import About from "./Components/About/About";
-import Technologies from "./components/Technologies/technologies";
-import Projects from "./Components/Projects/projects";
-import Certifications from "./Components/certifications/certifications";
-import Contact from "./Components/Contacts/contacts";
+import Technologies from "./Components/Technologies/Technologies";
+import Projects from "./Components/Projects/Projects";
+import Certifications from "./Components/certifications/Certifications";
+import Contact from "./Components/Contacts/Contacts";
 import "./App.css";
 
 function App() {
